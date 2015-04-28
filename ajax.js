a970cb7d825cae5037cf1e22316abf885b06abf0
LittleXiaosi <yansi1993@126.com>
2015-04-28 17:50:00 +0800

    添加对post的支持
    添加对post支持，修复部分bug

--- ajax.js
+++ ajax.js
@@ -3,16 +3,15 @@
  * */
 
 (function() {
-    var that;
 
     function AJAX(obj) {
-        that = this;
+        var that = this;
         that.argu = {
             type: 'GET',
             url: window.location.origin + '/H5Event/AppQd/work/action/',
             action: "",
             data: {
-                signIn: '374179084'
+                //signIn: ''
             },
             callback: function() {
             }
@@ -23,6 +22,7 @@
         }
         that.http_request = false;
 
+        console.log(that.argu.action + 'ajax1');
         that.init();
     }
 
@@ -30,6 +30,10 @@
 
         //初始化当前请求
         init: function() {
+            var that = this;
+
+            console.log(that.argu.action + 'ajax2');
+
             if (window.XMLHttpRequest) { // Mozilla, Safari,...
                 that.http_request = new XMLHttpRequest();
                 if (that.http_request.overrideMimeType) {
@@ -50,25 +54,57 @@
                 that.console('Giving up :( Cannot create an XMLHTTP instance');
                 return false;
             }
-            that.http_request.onreadystatechange = that.consoleContents;
-            that.http_request.open('GET', that.getUrlData(), true);
-            that.http_request.send();
+
+            console.log(that.argu.action + 'ajax3');
+            that.http_request.onreadystatechange = function() {
+                that.consoleContents();
+            };
+            that.http_request.async = !that.argu.sync;
+            if (that.argu.type.toLocaleLowerCase() == 'get') {
+                that.http_request.open('GET', that.getUrlData(), true);
+                that.http_request.send();
+            } else if (that.argu.type.toLocaleLowerCase() == 'post') {
+                that.http_request.open('POST', that.getUrl(), true);
+                that.http_request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                console.log(that.argu.key + '=' + that.getJsonData(that.argu.data));
+                that.http_request.send(that.argu.key + '=' + encodeURIComponent(that.getJsonData(that.argu.data)));
+            }
         },
-        //拼合参数与请求地址
+        //get请求拼合参数与请求地址
         getUrlData: function() {
+            var that = this;
+
             var url = that.argu.url;
             var data = [];
             for (var key in that.argu.data) {
-                var value = that.argu.data[key];
-                data.push(key + '=' + encodeURIComponent(value));
+                if(that.argu.data[key]) {
+                    var value = that.argu.data[key];
+                    data.push(key + '=' + encodeURIComponent(value));
+                }
             }
-            if (data.length) {
+            if (data.length > 0) {
                 url += that.argu.action + '?' + data.join('&');
+            } else {
+                url += that.argu.action;
             }
             return url;
         },
+        //post请求获取请求地址
+        getUrl: function() {
+            var that = this;
+
+            var url = that.argu.url;
+            return url;
+        },
+        //post请求json格式转化
+        getJsonData: function(data) {
+            var json = JSON.stringify(data);
+            return json;
+        },
         //输出请求结果
         consoleContents: function() {
+            var that = this;
+
             if (that.http_request.readyState == 4) {
                 if (that.http_request.status == 200) {
                     that.argu.callback.call(that, JSON.parse(that.http_request.responseText));
